f54de0c2f0d32070151bf24dd126173222e6a131
Eh576 <Eh576@kent.ac.uk>
2026-02-10 02:34:49 +0000

    fleet_manager.py

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,1 +1,6 @@
-##Commit test 
+def init_database(): 
+    names = ["Edon", "James", "Joseph", "Turk", "Levi"]
+    ranks = ["Captain", "Commander", "Lieutenant", "Captain", "Lt. Commander"]
+    divs = ["Command", "Operations", "Security", "Operations", "Command"]
+    ids = ["6701, 6702, 6703, 6704, 6705"] ## Sorry about 67 
+    return names, ranks, divs, ids
